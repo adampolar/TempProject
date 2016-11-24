2d40962edf2eaae6cda1af5673aa89d395372b8f
adampolar <a>
2016-11-24 08:49:53 +0000

    changed rules in assignment eligibility

--- BAURotaService/BAUAssignmentEligibilitySelector.cs
+++ BAURotaService/BAUAssignmentEligibilitySelector.cs
@@ -7,10 +7,10 @@ namespace BAURotaService
 {
     public class BAUAssignmentEligibilitySelector : IBAUAssignmentEligibilitySelector
     {
-        public List<int> ListEligibleEmployees(DateTime date, bool morning, List<BAUAssignment> LastTwoWeeksAssignments, List<int> allEmployees)
+        public List<int> ListEligibleEmployees(DateTime date, bool morning, List<BAUAssignment> lastTwoWeeksAssignmentsOrMore, List<int> allEmployees)
         {
             IEnumerable<int> eligibleEmployees = allEmployees;
-
+            /*
             //ensure any developers who haven't done bau in 2 weeks are selected
             //has to be thought of going forward
             //for example if there are 6 people that havent done any shifts in the last (2 weeks - 3 days) then only they can be selected
@@ -31,12 +31,65 @@ namespace BAURotaService
                         break;
                     }
                 }
+            }*/
+
+
+            var lastSunday = date.AddDays(-(int)date.DayOfWeek - 7);
+            var assignmentsInLastTwoCalendarWeeks = lastTwoWeeksAssignmentsOrMore.Where(a => a.Date > lastSunday);
+            var employeesWithAssignmentCounts = assignmentsInLastTwoCalendarWeeks
+                .GroupBy(a => a.EmployeeNumber)
+                .Select(g => new { EmployeeNumber = g.Key, Count = g.Count() });
+
+            //add in employees with no assignments (probably a better way of doing this
+            foreach (var empNo in allEmployees)
+            {
+                if (employeesWithAssignmentCounts.Where(c => c.EmployeeNumber == empNo).Count() == 0)
+                {
+                    var list = employeesWithAssignmentCounts.ToList();
+                    list.Add( new { EmployeeNumber = empNo, Count = 0 });
+                    employeesWithAssignmentCounts = list;
+                }
             }
-            
+
+            var employeesWhoHaveHadLessThan2Assignments = employeesWithAssignmentCounts
+                .Where(a => a.Count < 2);
+
+
+            //i think theres a special case around if you have 2 left with 0 assignments 
+            // and 2 left with 1 assignments then pick the 2 without assignments be
+            // stems from worse case scenario of ab,cd,ab,cd,ef,gh,ef,
+            // then you need to pick ij
+
+            if (assignmentsInLastTwoCalendarWeeks.Count() >= 14)
+            {
+                if (employeesWhoHaveHadLessThan2Assignments.Where(a => a.Count == 0).Count() == 2
+                    && employeesWhoHaveHadLessThan2Assignments.Where(a => a.Count == 1).Count() == 2)
+                {
+                    return employeesWhoHaveHadLessThan2Assignments
+                        .Where(a => a.Count == 0)
+                        .Select(a => a.EmployeeNumber)
+                        .ToList();
+                }
+
+                //also for that afternoon
+                if (employeesWhoHaveHadLessThan2Assignments.Where(a => a.Count == 0).Count() == 1
+                    && employeesWhoHaveHadLessThan2Assignments.Where(a => a.Count == 1).Count() == 3)
+                {
+                    return employeesWhoHaveHadLessThan2Assignments
+                        .Where(a => a.Count == 0)
+                        .Select(a => a.EmployeeNumber)
+                        .ToList(); //boring wheel of fortune, only one employee
+                }
+            }
+
+            eligibleEmployees = employeesWhoHaveHadLessThan2Assignments.Select(a => a.EmployeeNumber);
+
+
+
             //select developers who are not in the other half of the day
 
             eligibleEmployees = eligibleEmployees.Where(x =>
-            LastTwoWeeksAssignments.Where(
+            lastTwoWeeksAssignmentsOrMore.Where(
                 a =>
                 (
                 a.Date == date &&
@@ -50,7 +103,7 @@ namespace BAURotaService
             // who are not on the day previous
 
             eligibleEmployees = eligibleEmployees.Where(x =>
-            LastTwoWeeksAssignments.Where(
+            lastTwoWeeksAssignmentsOrMore.Where(
                 a =>
                 (
                 a.Date == date - new TimeSpan(1, 0, 0, 0) &&
--- StaffBAURotaService.Test/BAUAssignmentEligibilitySelectorTest.cs
+++ StaffBAURotaService.Test/BAUAssignmentEligibilitySelectorTest.cs
@@ -13,28 +13,29 @@ namespace StaffBAURotaService.Test
     {
         private List<BAUAssignment> contrivedAssignments = new List<BAUAssignment>()
         {
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(1, 0, 0, 0), Morning = false, EmployeeNumber = 10 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(1, 0, 0, 0), Morning = true, EmployeeNumber = 9 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(2, 0, 0, 0), Morning = false, EmployeeNumber = 8 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(2, 0, 0, 0), Morning = true, EmployeeNumber = 7 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(3, 0, 0, 0), Morning = false, EmployeeNumber = 6 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(3, 0, 0, 0), Morning = true, EmployeeNumber = 5 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(4, 0, 0, 0), Morning = false, EmployeeNumber = 4 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(4, 0, 0, 0), Morning = true, EmployeeNumber = 3 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(7, 0, 0, 0), Morning = false, EmployeeNumber = 2 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(7, 0, 0, 0), Morning = true, EmployeeNumber = 1 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(8, 0, 0, 0), Morning = false, EmployeeNumber = 4 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(8, 0, 0, 0), Morning = true, EmployeeNumber = 3 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(9, 0, 0, 0), Morning = false, EmployeeNumber = 2 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(9, 0, 0, 0), Morning = true, EmployeeNumber = 1 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(10, 0, 0, 0), Morning = false, EmployeeNumber = 4 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(10, 0, 0, 0), Morning = true, EmployeeNumber = 3 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(11, 0, 0, 0), Morning = false, EmployeeNumber = 2 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(11, 0, 0, 0), Morning = true, EmployeeNumber = 1 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(14, 0, 0, 0), Morning = false, EmployeeNumber = 4 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(14, 0, 0, 0), Morning = true, EmployeeNumber = 3 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(15, 0, 0, 0), Morning = false, EmployeeNumber = 2 },
-            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan(15, 0, 0, 0), Morning = true, EmployeeNumber = 1 },
+            //latest is thursday in the future
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 1, 0, 0, 0), Morning = false, EmployeeNumber = 8 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 1, 0, 0, 0), Morning = true, EmployeeNumber = 7 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 2, 0, 0, 0), Morning = false, EmployeeNumber = 10 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 2, 0, 0, 0), Morning = true, EmployeeNumber = 9 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 3, 0, 0, 0), Morning = false, EmployeeNumber = 6 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 3, 0, 0, 0), Morning = true, EmployeeNumber = 5 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 4, 0, 0, 0), Morning = false, EmployeeNumber = 8 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 4, 0, 0, 0), Morning = true, EmployeeNumber = 7 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 7, 0, 0, 0), Morning = false, EmployeeNumber = 6 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 7, 0, 0, 0), Morning = true, EmployeeNumber = 5 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 8, 0, 0, 0), Morning = false, EmployeeNumber = 4 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 8, 0, 0, 0), Morning = true, EmployeeNumber = 3 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 9, 0, 0, 0), Morning = false, EmployeeNumber = 2 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 9, 0, 0, 0), Morning = true, EmployeeNumber = 1 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 10, 0, 0, 0), Morning = false, EmployeeNumber = 4 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 10, 0, 0, 0), Morning = true, EmployeeNumber = 3 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 11, 0, 0, 0), Morning = false, EmployeeNumber = 2 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 11, 0, 0, 0), Morning = true, EmployeeNumber = 1 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 14, 0, 0, 0), Morning = false, EmployeeNumber = 4 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 14, 0, 0, 0), Morning = true, EmployeeNumber = 3 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 15, 0, 0, 0), Morning = false, EmployeeNumber = 2 },
+            new BAUAssignment() { Date=DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 15, 0, 0, 0), Morning = true, EmployeeNumber = 1 },
         };
 
         private List<BAUAssignment> randomAssignments = new List<BAUAssignment>()
@@ -70,69 +71,13 @@ namespace StaffBAURotaService.Test
         };
 
         [TestMethod]
-        public void TestIfOnly6SessionsLeftInTwoWeekPeriodAnd6PeopleAreLeftThenReturn6()
+        public void TestIfOnly6SessionsLeftInTwoWeekPeriodAndContrivedSituation()
         {
             //given 
-            var tstAss = contrivedAssignments.Skip(6).Take(20).ToList();
+            var tstAss = contrivedAssignments.Skip(4).Take(14).ToList();
             //when
             var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
-                DateTime.Now.Date - new TimeSpan(3, 0, 0, 0), true, 
-                tstAss, employees);
-            //then
-            CollectionAssert.AreEquivalent(new List<int>() { 5, 6, 7, 8, 9, 10 }, elEmps);
-
-        }
-
-        [TestMethod]
-        public void TestIfOnly5SessionsLeftInTwoWeekPeriodAnd5PeopleAreLeftThenReturn6()
-        {
-            //given 
-            var tstAss = contrivedAssignments.Skip(5).Take(20).ToList();
-            //when
-            var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
-                DateTime.Now.Date - new TimeSpan(3, 0, 0, 0), false,
-                tstAss, employees);
-            //then
-            CollectionAssert.AreEquivalent(new List<int>() { 6, 7, 8, 9, 10 }, elEmps);
-
-        }
-
-        [TestMethod]
-        public void TestIfOnly4SessionsLeftInTwoWeekPeriodAnd4PeopleAreLeftThenReturn6()
-        {
-            //given 
-            var tstAss = contrivedAssignments.Skip(4).Take(20).ToList();
-            //when
-            var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
-                DateTime.Now.Date - new TimeSpan(2, 0, 0, 0), true,
-                tstAss, employees);
-            //then
-            CollectionAssert.AreEquivalent(new List<int>() { 7, 8, 9, 10 }, elEmps);
-
-        }
-
-        [TestMethod]
-        public void TestIfOnly3SessionsLeftInTwoWeekPeriodAnd3PeopleAreLeftThenReturn6()
-        {
-            //given 
-            var tstAss = contrivedAssignments.Skip(3).Take(20).ToList();
-            //when
-            var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
-                DateTime.Now.Date - new TimeSpan(2, 0, 0, 0), false,
-                tstAss, employees);
-            //then
-            CollectionAssert.AreEquivalent(new List<int>() { 8, 9, 10 }, elEmps);
-
-        }
-
-        [TestMethod]
-        public void TestIfOnly2SessionsLeftInTwoWeekPeriodAnd2PeopleAreLeftThenReturn6()
-        {
-            //given 
-            var tstAss = contrivedAssignments.Skip(2).Take(20).ToList();
-            //when
-            var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
-                DateTime.Now.Date - new TimeSpan(1, 0, 0, 0), true,
+                DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 2, 0, 0, 0), true, 
                 tstAss, employees);
             //then
             CollectionAssert.AreEquivalent(new List<int>() { 9, 10 }, elEmps);
@@ -140,13 +85,13 @@ namespace StaffBAURotaService.Test
         }
 
         [TestMethod]
-        public void TestIfOnly1SessionsLeftInTwoWeekPeriodAnd1PeopleAreLeftThenReturn6()
+        public void TestIfOnly5SessionsLeftInTwoWeekPeriodAndContrivedSituation()
         {
-            //given
-            var tstAss = contrivedAssignments.Skip(1).Take(20).ToList();
+            //given 
+            var tstAss = contrivedAssignments.Skip(3).Take(15).ToList();
             //when
             var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
-                DateTime.Now.Date - new TimeSpan(1, 0, 0, 0), false,
+                DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 2, 0, 0, 0), false,
                 tstAss, employees);
             //then
             CollectionAssert.AreEquivalent(new List<int>() { 10 }, elEmps);
@@ -154,16 +99,60 @@ namespace StaffBAURotaService.Test
         }
 
         [TestMethod]
+        public void TestIfOnly4SessionsLeftInTwoWeekPeriodAndContrivedSituation()
+        {
+            //given 
+            var tstAss = contrivedAssignments.Skip(2).Take(20).ToList();
+            //when
+            var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
+                DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 1, 0, 0, 0), false,
+                tstAss, employees);
+            //then
+            CollectionAssert.AreEquivalent(new List<int>() { 7, 8 }, elEmps);
+
+        }
+
+        [TestMethod]
+        public void TestIfOnly3SessionsLeftInTwoWeekPeriodAndContrivedSituation()
+        {
+            //given 
+            var tstAss = contrivedAssignments.Skip(1).Take(20).ToList();
+            //when
+            var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
+                DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5 + 1, 0, 0, 0), false,
+                tstAss, employees);
+            //then
+            CollectionAssert.AreEquivalent(new List<int>() { 8}, elEmps);
+
+        }
+
+        [TestMethod]
+        public void TestIfOnly2SessionsLeftInTwoWeekPeriodAndContrivedSituation()
+        {
+            //given 
+            var tstAss = contrivedAssignments.Take(20).ToList();
+            //when
+            var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
+                DateTime.Now.Date - new TimeSpan((int)DateTime.Now.DayOfWeek - 5, 0, 0, 0), false,
+                tstAss, employees);
+            //then
+            CollectionAssert.AreEquivalent(new List<int>() { 9, 10 }, elEmps);
+
+        }
+
+        [TestMethod]
         public void TestNoEmployeesOnConsecutiveDays()
         {
             //given
-            var tstAss = randomAssignments.ToList();
+            var tstAss = new List<BAUAssignment>();
+            tstAss.Add(new BAUAssignment() { Date = DateTime.Now.Date - new TimeSpan(1, 0, 0, 0), Morning = true, EmployeeNumber = 7 });
+            tstAss.Add(new BAUAssignment() { Date = DateTime.Now.Date - new TimeSpan(1, 0, 0, 0), Morning = false, EmployeeNumber = 8 });
             //when
             var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
                 DateTime.Now.Date, true,
                 tstAss, employees);
             //then
-            CollectionAssert.AreEquivalent(new List<int>() { 1,2,3,4,5,7,8,9 }, elEmps);
+            CollectionAssert.AreEquivalent(new List<int>() { 1,2,3,4,5,6,9,10 }, elEmps);
         }
 
         [TestMethod]
@@ -171,14 +160,13 @@ namespace StaffBAURotaService.Test
         {
             //given
             var tstAss = new List<BAUAssignment>();
-            tstAss.AddRange(randomAssignments);
             tstAss.Add(new BAUAssignment() { Date = DateTime.Now.Date, Morning = true, EmployeeNumber = 7 });
             //when
             var elEmps = new BAUAssignmentEligibilitySelector().ListEligibleEmployees(
                 DateTime.Now.Date, false,
                 tstAss, employees);
             //then
-            CollectionAssert.AreEquivalent(new List<int>() { 1, 2, 3, 4, 5, 8, 9 }, elEmps);
+            CollectionAssert.AreEquivalent(new List<int>() { 1, 2, 3, 4, 5, 6, 8, 9, 10 }, elEmps);
         }
 
     }
